c78895b9c4bd6d961701f1acd6d63dbdeb03a394
Tomáš Jílek <tomas.jilek@morosystems.cz>
2014-12-19 10:52:00 +0100

    no message

--- src/pirozekenum/PirozekEnum.java
+++ src/pirozekenum/PirozekEnum.java
@@ -4,9 +4,14 @@
  */
 package pirozekenum;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import pirozekenum.Message.AbstractMessage;
+import pirozekenum.Message.Message1;
+import pirozekenum.Message.Message2;
+import pirozekenum.Message.Message3;
 
 /**
  *
@@ -20,9 +25,25 @@ public class PirozekEnum {
     public static void main(String[] args) {
         String code = "zprava1";
 
-        TypEnum en = TypEnum.getByCode(code);
+        //Pomoci enumu
         try {
+            TypEnum en = TypEnum.getByCode(code);
             AbstractMessage inst = en.getCls().newInstance();
+            System.out.println(inst);
+        } catch (Exception ex) {
+            Logger.getLogger(PirozekEnum.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //pomoci mapy
+        Map<String, Class<? extends AbstractMessage>> mapa = new HashMap<>();
+        mapa.put("zprava1", Message1.class);
+        mapa.put("zprava2", Message2.class);
+        mapa.put("zprava3", Message3.class);
+
+        try {
+            Class<? extends AbstractMessage> cls = mapa.get(code);
+            AbstractMessage inst = cls.newInstance();
+            System.out.println(inst);
         } catch (Exception ex) {
             Logger.getLogger(PirozekEnum.class.getName()).log(Level.SEVERE, null, ex);
         }
